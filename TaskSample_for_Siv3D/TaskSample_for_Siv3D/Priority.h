069f76ca455f6ca332bcd188d931e5be16859741
Rinifisu <rinifisu@outlook.jp>
2016-03-07 00:54:56 +0900

    no message

--- TaskSample_for_Siv3D/TaskSample_for_Siv3D/Priority.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-//タスクの更新と描画の優先順位
-enum class Priority
-{
-	Ring,
-	Ball,
-};
--- TaskSample_for_Siv3D/TaskSample_for_Siv3D/rnfs.h
+++
@@ -1,8 +0,0 @@
-﻿//Copyright © 2015-2016 Rinifisu
-//http://rinifisu.blog.jp/
-
-#pragma once
-
-#include "rnfs/rnfs_TaskCall.h"
-#include "rnfs/rnfs_TaskGet.h"
-#include "rnfs/rnfs_TaskSend.h"
