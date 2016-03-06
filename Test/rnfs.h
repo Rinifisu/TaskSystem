543a7f82c2bbb636a782588840f9df75f20f6c44
Rinifisu <rinifisu@outlook.jp>
2016-03-07 01:47:58 +0900

    no message

--- Test/rnfs.h
+++
@@ -1,8 +0,0 @@
-﻿//Copyright © 2015-2016 Rinifisu
-//http://rinifisu.blog.jp/
-
-#pragma once
-
-#include "TaskSystem/rnfs_TaskCall.h"
-#include "TaskSystem/rnfs_TaskGet.h"
-#include "TaskSystem/rnfs_TaskSend.h"
